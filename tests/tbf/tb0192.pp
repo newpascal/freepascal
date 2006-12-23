newpascal/freepascal

commit 31727cf909be44996e31721684d27e7532bfeaa2

* fixed mantis 7173 (properly check parameters in unit     implementation in Delphi/TP mode if they are repeated)
git-svn-id: http://svn.freepascal.org/svn/fpc/trunk@5682 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/pdecsub.pas
@@ -2457,7 +2457,6 @@ const
                      also the parameters must match also with the type }
                    if (fwpd.proctypeoption<>currpd.proctypeoption) or
                       (
-                       (m_repeat_forward in current_settings.modeswitches) and
                        (not((currpd.maxparacount=0) or
                             (compare_paras(currpd.paras,fwpd.paras,cp_all,[cpo_comparedefaultvalue])>=te_equal)))
                       ) or

tests/webtbs/tw7173.pp
@@ -1,0 +1,23 @@
+{ %norun }
+{ %fail }
+
+{$mode delphi}
+unit tw7173;
+
+interface
+
+uses
+  SysUtils;
+
+function GetVal(astr : string) : Integer;
+
+
+implementation
+
+function GetVal(const astr : string) : Integer;
+begin
+  Result := StrToInt(astr);
+end;
+
+
+end.

commit 0b07bc8731821447f1f5d6d58596a44c5a7781d9

* fixed one regression in r5682: implementation declarations with a     calling convention that has different hidden parameters than the     interface declaration no longer compiled in Delphi mode (e.g.     webtbs/tw7329.pp on i386)   * fixed remaining declaration parsing incompatibilities in TP/Delphi     modes (other modes already gave errors for the things below):     * give an error for "function a: byte;" in interface followed by       "procedure a;" in implementation ("function a;" in implementation       still allowed as in TP/Delphi)     * give an error for "function a(b: byte):byte" in interface       followed by "function a: byte;" in implementation (if one parameter       or return type is specified in implementation, everything must       be repeated -- "function a;" still allowed)   * copied webtbs/tw0890.pp to webtbf/tw0890a.pp since it now correctly     fails, and modified webtbs/tw0890.pp so it doesn't fail with the     new code
git-svn-id: http://svn.freepascal.org/svn/fpc/trunk@5688 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/pdecsub.pas
@@ -918,6 +918,7 @@ implementation
                   { pd=nil when it is a interface mapping }
                   if assigned(pd) then
                     begin
+                      include(pd.procoptions,po_function);
                       if try_to_consume(_COLON) then
                        begin
                          inc(testcurobject);
@@ -2408,8 +2409,9 @@ const
         curridx,
         fwidx,
         i       : longint;
-        forwardfound : boolean;
         po_comp : tprocoptions;
+        paracompopt: tcompare_paras_options;
+        forwardfound : boolean;
       begin
         forwardfound:=false;
 
@@ -2453,37 +2455,11 @@ const
                  begin
                    forwardfound:=true;
 
-                   { Check if the procedure type and return type are correct,
-                     also the parameters must match also with the type }
-                   if (fwpd.proctypeoption<>currpd.proctypeoption) or
-                      (
-                       (not((currpd.maxparacount=0) or
-                            (compare_paras(currpd.paras,fwpd.paras,cp_all,[cpo_comparedefaultvalue])>=te_equal)))
-                      ) or
-                      (
-                       ((m_repeat_forward in current_settings.modeswitches) or
-                        not(is_void(currpd.returndef))) and
-                       (not equal_defs(fwpd.returndef,currpd.returndef))) then
-                     begin
-                       MessagePos1(currpd.fileinfo,parser_e_header_dont_match_forward,
-                                   currpd.fullprocname(false));
-                       tprocsym(currpd.procsym).write_parameter_lists(currpd);
-                       break;
-                     end;
-
-                   { Check if both are declared forward }
-                   if fwpd.forwarddef and currpd.forwarddef then
-                    begin
-                      MessagePos1(currpd.fileinfo,parser_e_function_already_declared_public_forward,
-                                  currpd.fullprocname(false));
-                    end;
-
-                   { internconst or internproc only need to be defined once }
-                   if (fwpd.proccalloption=pocall_internproc) then
-                    currpd.proccalloption:=fwpd.proccalloption
+                   if (m_repeat_forward in current_settings.modeswitches) or
+                      (fwpd.proccalloption<>currpd.proccalloption) then
+                     paracompopt:=[cpo_ignorehidden,cpo_comparedefaultvalue]
                    else
-                    if (currpd.proccalloption=pocall_internproc) then
-                     fwpd.proccalloption:=currpd.proccalloption;
+                     paracompopt:=[cpo_comparedefaultvalue];
 
                    { Check calling convention }
                    if (fwpd.proccalloption<>currpd.proccalloption) then
@@ -2516,12 +2492,40 @@ const
                         end;
                     end;
 
+                   { Check if the procedure type and return type are correct,
+                     also the parameters must match also with the type }
+                   if ((m_repeat_forward in current_settings.modeswitches) or
+                       (currpd.maxparacount<>0) or
+                       (not(is_void(currpd.returndef)))) and
+                      ((compare_paras(currpd.paras,fwpd.paras,cp_all,paracompopt)<te_equal) or
+                       (not equal_defs(fwpd.returndef,currpd.returndef))) then
+                     begin
+                       MessagePos1(currpd.fileinfo,parser_e_header_dont_match_forward,
+                                   fwpd.fullprocname(false));
+                       tprocsym(currpd.procsym).write_parameter_lists(currpd);
+                       break;
+                     end;
+
+                   { Check if both are declared forward }
+                   if fwpd.forwarddef and currpd.forwarddef then
+                    begin
+                      MessagePos1(currpd.fileinfo,parser_e_function_already_declared_public_forward,
+                                  currpd.fullprocname(false));
+                    end;
+
+                   { internconst or internproc only need to be defined once }
+                   if (fwpd.proccalloption=pocall_internproc) then
+                    currpd.proccalloption:=fwpd.proccalloption
+                   else
+                    if (currpd.proccalloption=pocall_internproc) then
+                     fwpd.proccalloption:=currpd.proccalloption;
+
                    { Check procedure options, Delphi requires that class is
                      repeated in the implementation for class methods }
                    if (m_fpc in current_settings.modeswitches) then
-                     po_comp:=[po_classmethod,po_varargs,po_methodpointer,po_interrupt]
+                     po_comp:=[po_classmethod,po_varargs,po_methodpointer,po_interrupt,po_function]
                    else
-                     po_comp:=[po_classmethod,po_methodpointer];
+                     po_comp:=[po_classmethod,po_methodpointer,po_function];
 
                    if ((po_comp * fwpd.procoptions)<>(po_comp * currpd.procoptions)) then
                      begin

compiler/symconst.pas
@@ -271,7 +271,9 @@ type
     { importing }
     po_has_importdll,
     po_has_importname,
-    po_kylixlocal
+    po_kylixlocal,
+    { this is a function and not a procedure -- needed for tbf/tb0175 }
+    po_function
   );
   tprocoptions=set of tprocoption;
 

tests/tbf/tb0191.pp
@@ -1,0 +1,20 @@
+{ %norun }
+{ %fail }
+{$mode delphi}
+unit tb0191;
+
+interface
+
+type
+  tii = interface(iunknown) end;
+  ti2 = interface(tii) end;
+
+function a(b: longint): tii; stdcall;
+
+implementation
+
+function a:tii;
+begin
+end;
+
+end.

tests/tbf/tb0192.pp
@@ -1,0 +1,20 @@
+{ %norun }
+{ %fail }
+{$mode delphi}
+unit tb0192;
+
+interface
+
+type
+  tii = interface(iunknown) end;
+  ti2 = interface(iunknown) end;
+
+function a: tii; stdcall;
+
+implementation
+
+function a:ti2;
+begin
+end;
+
+end.

tests/tbs/tb0517.pp
@@ -1,0 +1,19 @@
+{ %norun }
+{$mode delphi}
+unit tb0517;
+
+interface
+
+type
+  tii = interface(iunknown) end;
+  ti2 = interface(iunknown) end;
+
+function a(b: longint): tii; stdcall;
+
+implementation
+
+function a;
+begin
+end;
+
+end.

tests/webtbf/tw0890a.pp
@@ -1,3 +1,4 @@
+{ %fail }
 {$ifdef FPC}
   {$MODE TP}
 {$endif FPC}
@@ -8,6 +9,7 @@ INTERFACE
 
 procedure GetScreenLine(const x: Integer);
 
+function dummy(const x : integer) : integer;
 function dummy2(var x : integer) : integer;
 function dummystr(x : integer) : string;
 
@@ -30,6 +32,12 @@ var
 begin
   str(x,s);
   dummystr:=s;
+end;
+
+{ this one is refused by BP :( }
+function dummy : integer;
+begin
+  dummy:=x;
 end;
 
 begin