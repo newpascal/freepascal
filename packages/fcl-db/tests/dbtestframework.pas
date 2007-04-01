ac7a7c149def54d309138a051ec20430456ffc98
joost <joost@3ad0048d-3df7-0310-abae-a5850022a9f2>
2007-04-01 15:18:21 +0000

    * Renamed too long testnames to shorter ones  * Added TestSetBlobAsStringParam and TestSetBlobAsMemoParam tests  * Fixed TestpfInUpdateFlag test
    git-svn-id: http://svn.freepascal.org/svn/fpc/trunk@7051 3ad0048d-3df7-0310-abae-a5850022a9f2

--- packages/fcl-db/tests/dbtestframework.pas
+++ packages/fcl-db/tests/dbtestframework.pas
@@ -4,27 +4,51 @@ program dbtestframework;
   {$mode objfpc}{$H+}
 {$ENDIF}
 
+{ $DEFINE STOREDB}
+
 {$APPTYPE CONSOLE}
 
 uses
   SysUtils,
-  fpcunit, testregistry, testreport,
+  fpcunit,  testreport, testregistry,
+{$IFDEF STOREDB}
+  DBResultsWriter,
+{$ENDIF}
   toolsunit,
+// Units wich contains the tests
   testbasics, testsqlfieldtypes, testdbbasics;
   
 var
   FXMLResultsWriter: TXMLResultsWriter;
+{$IFDEF STOREDB}
+  FDBResultsWriter: TDBResultsWriter;
+{$ENDIF}
   testResult: TTestResult;
 begin
   testResult := TTestResult.Create;
   FXMLResultsWriter := TXMLResultsWriter.Create;
+{$IFDEF STOREDB}
+  FDBResultsWriter := TDBResultsWriter.Create;
+{$ENDIF}
   try
     testResult.AddListener(FXMLResultsWriter);
+{$IFDEF STOREDB}
+    testResult.AddListener(FDBResultsWriter);
+{$ENDIF}
     FXMLResultsWriter.WriteHeader;
+{$IFDEF STOREDB}
+    FDBResultsWriter.OpenConnection(dbconnectorname+';'+dbconnectorparams);
+{$ENDIF}
     GetTestRegistry.Run(testResult);
     FXMLResultsWriter.WriteResult(testResult);
+{$IFDEF STOREDB}
+    FDBResultsWriter.CloseConnection;
+{$ENDIF}
   finally
     testResult.Free;
     FXMLResultsWriter.Free;
+{$IFDEF STOREDB}
+    FDBResultsWriter.Free;
+{$ENDIF}
   end;
 end.
