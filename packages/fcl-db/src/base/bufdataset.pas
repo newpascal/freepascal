newpascal/freepascal

commit c24639adc5c38edf99a0713747486601abe0e61d

* Added OnValidate event support (17510)
git-svn-id: http://svn.freepascal.org/svn/fpc/trunk@17220 3ad0048d-3df7-0310-abae-a5850022a9f2

packages/fcl-db/src/base/fields.inc
@@ -820,7 +820,15 @@ procedure TField.SetData(Buffer: Pointer; NativeFormat : Boolean);
 begin
   If Not Assigned(FDataset) then
     DatabaseErrorFmt(SNoDataset,[FieldName]);
+  if (FieldNo>0) and not (FDataSet.State in [dsSetKey, dsFilter]) then
+    begin
+    if ReadOnly then 
+      DatabaseErrorFmt(SReadOnlyField, [DisplayName], Self); 
+    Validate(Buffer);
+    end;
   FDataSet.SetFieldData(Self,Buffer, NativeFormat);
+  if not (FDataSet.State in [dsCalcFields, dsFilter, dsNewValue]) then
+    DataChanged;
 end;
 
 Procedure TField.SetDataset (AValue : TDataset);

commit 02f81e81aaabeefef9dec764df4d9a43cfb68510

* move validate to newvalue assignment, fixes regression in non fcl-db datasets     caused by r17220. Patch by Luiz Americo, Mantis #19313
git-svn-id: http://svn.freepascal.org/svn/fpc/trunk@18872 3ad0048d-3df7-0310-abae-a5850022a9f2

packages/fcl-db/src/base/bufdataset.pas
@@ -1859,14 +1859,15 @@ var CurrBuff : pointer;
     NullMask : pbyte;
 
 begin
-  if not (state in [dsEdit, dsInsert, dsFilter, dsCalcFields]) then
-    begin
-    DatabaseErrorFmt(SNotEditing,[Name],self);
-    exit;
-    end;
+  if not (State in dsWriteModes) then
+    DatabaseError(SNotEditing, Self);
   CurrBuff := GetCurrentBuffer;
   If Field.Fieldno > 0 then // If = 0, then calculated field or something
     begin
+    if Field.ReadOnly and not (State in [dsSetKey, dsFilter]) then
+      DatabaseErrorFmt(SReadOnlyField, [Field.DisplayName]);	
+    if State in [dsEdit, dsInsert, dsNewValue] then
+      Field.Validate(Buffer);	
     NullMask := CurrBuff;
 
     inc(CurrBuff,FFieldBufPositions[Field.FieldNo-1]);

packages/fcl-db/src/base/fields.inc
@@ -820,12 +820,6 @@ procedure TField.SetData(Buffer: Pointer; NativeFormat : Boolean);
 begin
   If Not Assigned(FDataset) then
     DatabaseErrorFmt(SNoDataset,[FieldName]);
-  if (FieldNo>0) and not (FDataSet.State in [dsSetKey, dsFilter]) then
-    begin
-    if ReadOnly then 
-      DatabaseErrorFmt(SReadOnlyField, [DisplayName], Self); 
-    Validate(Buffer);
-    end;
   FDataSet.SetFieldData(Self,Buffer, NativeFormat);
 end;
 

packages/fcl-db/src/dbase/dbf.pas
@@ -764,6 +764,8 @@ var
 begin
   if (Field.FieldNo >= 0) then
   begin
+    if State in [dsEdit, dsInsert, dsNewValue] then
+      Field.Validate(Buffer);
     Dst := @PDbfRecord(ActiveBuffer)^.DeletedFlag;
     FDbfFile.SetFieldData(Field.FieldNo - 1, Field.DataType, Buffer, Dst, NativeFormat);
   end else begin    { ***** fkCalculated, fkLookup ***** }

packages/fcl-db/src/memds/memds.pp
@@ -728,6 +728,8 @@ begin
  I:= Field.FieldNo - 1;
  if (I >= 0) and  MDSGetActiveBuffer(DestBuffer) then 
    begin
+   if State in [dsEdit, dsInsert, dsNewValue] then
+      Field.Validate(Buffer);
    if buffer = nil then 
      setfieldisnull(pointer(destbuffer),I)
    else 

packages/fcl-db/src/sdf/sdfdata.pp
@@ -128,7 +128,7 @@ How to Install
 interface
 
 uses
-  DB, Classes, SysUtils;
+  DB, Classes, SysUtils, DBConst;
 
 type
 //-----------------------------------------------------------------------------
@@ -664,16 +664,17 @@ var
   RecBuf, BufEnd: PChar;
   p : Integer;
 begin
-  if not (State in [dsEdit, dsInsert]) then
-    DatabaseError('Dataset not in edit or insert mode', Self);
+  if not (State in dsWriteModes) then
+    DatabaseError(SNotEditing, Self);
   GetActiveRecBuf(RecBuf);
   if Field.FieldNo > 0 then
   begin
     if State = dsCalcFields then
       DatabaseError('Dataset not in edit or insert mode', Self);
     if Field.ReadOnly and not (State in [dsSetKey, dsFilter]) then
-      DatabaseErrorFmt('Field ''%s'' cannot be modified', [Field.DisplayName]);
-    Field.Validate(Buffer);
+      DatabaseErrorFmt(SReadOnlyField, [Field.DisplayName]);
+    if State in [dsEdit, dsInsert, dsNewValue] then
+      Field.Validate(Buffer);
     if Field.FieldKind <> fkInternalCalc then
     begin
       SetFieldPos(RecBuf, Field.FieldNo);