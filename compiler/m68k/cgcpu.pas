newpascal/freepascal

commit 6d823d740b71e173b976b5bf170413d5e2384465

m68k: hopefully fix passing of smaller-than-alignment sized records/structs for stdcall/cdecl
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@36568 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/m68k/cgcpu.pas
@@ -384,8 +384,8 @@ unit cgcpu;
                 cgpara.check_simple_location;
                 len:=align(cgpara.intsize,cgpara.alignment);
                 g_stackpointer_alloc(list,len);
-                reference_reset_base(href,NR_STACK_POINTER_REG,0,cgpara.alignment,[]);
-                g_concatcopy(list,r,href,len);
+                reference_reset_base(href,NR_STACK_POINTER_REG,cgpara.location^.reference.offset,cgpara.alignment,[]);
+                g_concatcopy(list,r,href,cgpara.intsize);
               end
             else
               begin

compiler/m68k/cpupara.pas
@@ -343,10 +343,10 @@ unit cpupara;
                   begin
                     paraloc^.reference.index:=NR_FRAME_POINTER_REG;
                     inc(paraloc^.reference.offset,target_info.first_parm_offset);
-                    { M68K is a big-endian target }
-                    if (paralen<target_info.stackalign{tcgsize2size[OS_INT]}) then
-                      inc(paraloc^.reference.offset,target_info.stackalign-paralen);
                   end;
+                { M68K is a big-endian target }
+                if (paralen<target_info.stackalign{tcgsize2size[OS_INT]}) then
+                  inc(paraloc^.reference.offset,target_info.stackalign-paralen);
                 inc(cur_stack_offset,align(paralen,target_info.stackalign));
                 paralen := 0;
 

commit 14c9ceec7eec42784e080c60a88c62d64d1f9031

m68k: try to fix some breakages caused by r36568
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@36569 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/m68k/cgcpu.pas
@@ -372,6 +372,7 @@ unit cgcpu;
 
       var
         len : tcgint;
+        ofs : tcgint;
         href : treference;
       begin
         { cgpara.size=OS_NO requires a copy on the stack }
@@ -384,7 +385,10 @@ unit cgcpu;
                 cgpara.check_simple_location;
                 len:=align(cgpara.intsize,cgpara.alignment);
                 g_stackpointer_alloc(list,len);
-                reference_reset_base(href,NR_STACK_POINTER_REG,cgpara.location^.reference.offset,cgpara.alignment,[]);
+                ofs:=0;
+                if cgpara.intsize<target_info.stackalign then
+                  ofs:=target_info.stackalign-cgpara.intsize;
+                reference_reset_base(href,NR_STACK_POINTER_REG,ofs,cgpara.alignment,[]);
                 g_concatcopy(list,r,href,cgpara.intsize);
               end
             else

compiler/m68k/cpupara.pas
@@ -531,8 +531,8 @@ unit cpupara;
                                   varalign:=used_align(size_2_align(l),paraalign,paraalign);
                                   paraloc^.reference.offset:=cur_stack_offset;
                                   { M68K is a big-endian target }
-                                  if (paralen<tcgsize2size[OS_INT]) then
-                                    inc(paraloc^.reference.offset,4-paralen);
+                                  if (paralen<target_info.stackalign{tcgsize2size[OS_INT]}) then
+                                    inc(paraloc^.reference.offset,target_info.stackalign-paralen);
                                   if side=calleeside then
                                     inc(paraloc^.reference.offset,target_info.first_parm_offset);
                                   cur_stack_offset:=align(cur_stack_offset+l,varalign);