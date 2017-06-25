newpascal/freepascal

commit 6d823d740b71e173b976b5bf170413d5e2384465

m68k: hopefully fix passing of smaller-than-alignment sized records/structs for stdcall/cdecl
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@36568 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/m68k/cpupara.pas
@@ -343,10 +343,10 @@ unit cpupara;
                   begin
                     paraloc^.reference.index:=NR_FRAME_POINTER_REG;
                     inc(paraloc^.reference.offset,target_info.first_parm_offset);
-                    { M68K is a big-endian target }
-                    if (paralen<target_info.stackalign{tcgsize2size[OS_INT]}) then
-                      inc(paraloc^.reference.offset,target_info.stackalign-paralen);
                   end;
+                { M68K is a big-endian target }
+                if (paralen<target_info.stackalign{tcgsize2size[OS_INT]}) then
+                  inc(paraloc^.reference.offset,target_info.stackalign-paralen);
                 inc(cur_stack_offset,align(paralen,target_info.stackalign));
                 paralen := 0;
 

compiler/m68k/cgcpu.pas
@@ -384,8 +384,8 @@ unit cgcpu;
                 cgpara.check_simple_location;
                 len:=align(cgpara.intsize,cgpara.alignment);
                 g_stackpointer_alloc(list,len);
-                reference_reset_base(href,NR_STACK_POINTER_REG,0,cgpara.alignment,[]);
-                g_concatcopy(list,r,href,len);
+                reference_reset_base(href,NR_STACK_POINTER_REG,cgpara.location^.reference.offset,cgpara.alignment,[]);
+                g_concatcopy(list,r,href,cgpara.intsize);
               end
             else
               begin

commit 34217924a7a439310c36c519aa9fc7e54a9a781b

m68k: modify some alignment calculations in the register calling convention code, hopefully it fixes some Amiga regressions introduced in r36568
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@36596 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/m68k/cpupara.pas
@@ -535,8 +535,8 @@ unit cpupara;
                               paraloc^.reference.offset:=cur_stack_offset;
                               if side=calleeside then
                                 inc(paraloc^.reference.offset,target_info.first_parm_offset);
-                              if (paralen<target_info.stackalign{tcgsize2size[OS_INT]}) then
-                                inc(paraloc^.reference.offset,target_info.stackalign-paralen);
+                              if (paralen<target_info.stackalign) then
+                                inc(paraloc^.reference.offset,varalign-paralen);
                               cur_stack_offset:=align(cur_stack_offset+paralen,varalign);
                             end
                           else
@@ -577,8 +577,8 @@ unit cpupara;
                                   varalign:=used_align(size_2_align(l),paraalign,paraalign);
                                   paraloc^.reference.offset:=cur_stack_offset;
                                   { M68K is a big-endian target }
-                                  if (paralen<target_info.stackalign{tcgsize2size[OS_INT]}) then
-                                    inc(paraloc^.reference.offset,target_info.stackalign-paralen);
+                                  if (l<target_info.stackalign) then
+                                    inc(paraloc^.reference.offset,varalign-l);
                                   if side=calleeside then
                                     inc(paraloc^.reference.offset,target_info.first_parm_offset);
                                   cur_stack_offset:=align(cur_stack_offset+l,varalign);